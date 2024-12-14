7dcda1137d24e5db3aa55840d22f0c2d787c021c
Qudratjon1 <qudratovqudratjon5@gmail.com>
2024-12-14 17:16:34 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const hehe = ["waftoli", "bodrin", "baqlajon", "banan"];
+console.log("mevalar kopligi", hehe.ee);
+alert("mevalar kopligi" + hehe.ee);
+const userOO = confirm("Mevalardan bita meva obtawalsinmi");
+if (userOO) {
+    hehe.pop(); console.log("Yengilangan mevalar", hehe); alert("Meva obtawaldi Yengilangan mevalar" + hehe(""));
+} else {
+    hehe = "banan"; console.log("mevalar o'zicha qoldi", hehe); alert("mevalar o'zicha qoldi" + hehe(""));
+}
